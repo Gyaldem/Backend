0bb558ba5511ebf415fc7bf31803622153dc5bcb
lylia5 <ll_aouinine@esi.dz>
2024-02-09 20:54:51 +0100

    .

--- src/App.js
+++ src/App.js
@@ -7,44 +7,28 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const EventManagerCont = require('./controllers/EventManagerCont');
-const MentorCont = require('./controllers/MentorCont');
-const emailUtils=require('./utils/emailUtils');
-const {sendEmail}=emailUtils;
 
-const { AddParticipant, login } = EventManagerCont;
-const { MentorLogin, addMentor } = MentorCont; // Assuming MentorLogin is exported from MentorCont
+const generateSpacesRoute = require('./routes/EventmanagementRoute');
+const fetchDatafromDB=require('./routes/FetchdBRout')
+const{fetchParticipantsFromMongoDB,fetchJudgesFromMongoDB,fetchMentorsFromMongoDB,
+    fetchTeamsFromMongoDB} = require('./routes/FetchdBRout')
 
-// Middleware to parse JSON bodies
-app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/Db')
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-  });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+mongoose.connect("mongodb+srv://lylia:Q9MFU4dIajWfVZVC@auth.zrptolx.mongodb.net/?retryWrites=true&w=majority")
 
-const newParticipant = new Participant({
-  username: "hindooo",
-  teamId: "1",
-  email: "lh_dev@gmail.com",
-});
 
-// newParticipant.save()
-//   .then(result => {
-//     console.log('User added successfully:', result);
-//   })
-//   .catch(err => {
-//     console.error('here is the error' + err);
-//   });
-// Route handler for '/home'
-app.post('/home', (req, res) => {
-  console.log("hind");
-  addMentor(req,res); // Call MentorLogin function
-});
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
-// sendEmail("ll_aouinine@esi.dz")
+app.use('/', generateSpacesRoute);
+app.use('/',fetchDatafromDB)
+app.use('/',fetchJudgesFromMongoDB)
+app.use('/',fetchMentorsFromMongoDB)
+app.use('/',fetchTeamsFromMongoDB)
+
+app.listen(5000 , ()=>
+{
+    console.log('Connected to MongoDB');  
+})
+
